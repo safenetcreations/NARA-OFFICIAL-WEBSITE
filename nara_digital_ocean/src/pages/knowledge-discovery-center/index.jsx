7605e3fec1a9a38f2bcdc7a95a5e14b27192922b
nanthan77 <nanthan77@github.com>
2025-10-13 00:13:59 +0530

    fix: remove OceanSpaceHeader from knowledge-discovery and maritime pages
    - Remove duplicate OceanSpaceHeader from knowledge-discovery-center
    - Remove duplicate OceanSpaceHeader from maritime-services-hub
    - Now ALL pages use only global ThemeNavbar (no duplicates)
    - Fixes double navbar issue on these 2 pages

--- nara_digital_ocean/src/pages/knowledge-discovery-center/index.jsx
+++ nara_digital_ocean/src/pages/knowledge-discovery-center/index.jsx
@@ -1,11 +1,9 @@
 import React from 'react';
-import OceanSpaceHeader from '../../components/ui/OceanSpaceHeader';
 import KnowledgeDiscoveryRedesign from './KnowledgeDiscoveryRedesign';
 
 const KnowledgeDiscoveryCenter = () => {
   return (
     <>
-      <OceanSpaceHeader />
       <KnowledgeDiscoveryRedesign />
     </>
   );
--- nara_digital_ocean/src/pages/maritime-services-hub/index.jsx
+++ nara_digital_ocean/src/pages/maritime-services-hub/index.jsx
@@ -1,11 +1,9 @@
 import React from 'react';
-import OceanSpaceHeader from '../../components/ui/OceanSpaceHeader';
 import MaritimeServicesRedesign from './MaritimeServicesRedesign';
 
 const MaritimeServicesHub = () => {
   return (
     <>
-      <OceanSpaceHeader />
       <MaritimeServicesRedesign />
     </>
   );
