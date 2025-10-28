008e24f692d79c6accabd75f1ea577eb886fe605
safenetcreations <info@safenetcreations.com>
2025-10-28 23:00:11 +0530

    DELETE: Maritime Services Hub - Migrated features to Government Services Portal

--- nara_digital_ocean/src/Routes.jsx
+++ nara_digital_ocean/src/Routes.jsx
@@ -16,7 +16,6 @@ const ResearchExcellencePortal = lazy(() => import("./pages/research-excellence-
 const EmergencyResponseNetwork = lazy(() => import("./pages/emergency-response-network"));
 const LearningDevelopmentAcademy = lazy(() => import("./pages/learning-development-academy"));
 const RegionalImpactNetwork = lazy(() => import("./pages/regional-impact-network"));
-const MaritimeServicesHub = lazy(() => import("./pages/maritime-services-hub"));
 const MarineForecastPortal = lazy(() => import("./pages/marine-forecast/MarineForecastPortal"));
 const KnowledgeDiscoveryCenter = lazy(() => import("./pages/knowledge-discovery-center"));
 const PartnershipInnovationGateway = lazy(() => import("./pages/partnership-innovation-gateway"));
@@ -290,7 +289,6 @@ function Routes() {
               <Route path="/lda-register" element={<LDARegister />} />
               <Route path="/lda-login" element={<LDALogin />} />
               <Route path="/regional-impact-network" element={<RegionalImpactNetwork />} />
-              <Route path="/maritime-services-hub" element={<MaritimeServicesHub />} />
               <Route path="/marine-forecast" element={<MarineForecastPortal />} />
               <Route path="/live-ocean-data" element={<LiveOceanDataView />} />
               <Route path="/stormglass-maritime" element={<StormglassMaritimePage />} />
