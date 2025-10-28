39f8eb82c7bd20eb57b20c3912fe1c64cf7a89b9
safenetcreations <info@safenetcreations.com>
2025-10-29 01:10:58 +0530

    FIX: Remove require() from LiveVesselTracker - causing browser error

--- nara_digital_ocean/src/components/LiveVesselTracker.jsx
+++ nara_digital_ocean/src/components/LiveVesselTracker.jsx
@@ -4,14 +4,6 @@ import { Ship, Anchor, Navigation, Clock, Flag } from 'lucide-react';
 import 'leaflet/dist/leaflet.css';
 import L from 'leaflet';
 
-// Fix Leaflet default marker icon issue
-delete L.Icon.Default.prototype._getIconUrl;
-L.Icon.Default.mergeOptions({
-  iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
-  iconUrl: require('leaflet/dist/images/marker-icon.png'),
-  shadowUrl: require('leaflet/dist/images/marker-shadow.png'),
-});
-
 // Custom ship icon
 const createShipIcon = (shipType, course) => {
   const colors = {
